23170515215ae41e8b3c9101c0ca239b7b5421f8
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-14 06:33:13 +0530

    Modified Menubar.

--- app/dashboard/components/Menubar.tsx
+++ app/dashboard/components/Menubar.tsx
@@ -1,19 +1,21 @@
+import Link from "next/link";
 import React from "react";
-const names = ["Recent Orders", "Products Delivered", "Our Customers"];
+const names = [
+  { name: "AddProduct", path: "/dashboard/addProduct" },
+  { name: "ProductsDelivered", path: "/dashboard/productsDelivered" },
+  { name: "VeiwAllProducts", path: "/dashboard/veiwAllProducts" },
+];
 
 const Menubar = () => {
   return (
     <>
-      <div className="mx-auto max-w-5xl flex mt-8 text-2xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide">
-        {names.map((name, index) => (
-          <h2
-            key={index}
-            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-105 hover:text-pink-500 active:text-red-500">
-            {name}
-          </h2>
+      <div className="mx-auto max-w-5xl flex py-4 text-xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide px-3 sm:px-0 relative bg-zinc-100">
+        {names.map((item, index) => (
+          <Link href={item.path} key={index} className="cursor-pointer hover:text-pink-500">
+            {item.name}
+          </Link>
         ))}
       </div>
-      <div className=" absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"/>
     </>
   );
 };
