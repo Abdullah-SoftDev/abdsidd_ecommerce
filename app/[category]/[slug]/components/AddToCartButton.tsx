e8ece723d539a82b9573187fd02e25d6280b9d23
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-15 23:55:09 +0530

    Built remove from cart function.

--- app/[category]/[slug]/components/AddToCartButton.tsx
+++ app/[category]/[slug]/components/AddToCartButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { auth, db } from "@/firebase/firebaseConfig";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { deleteDoc, doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useDocument } from "react-firebase-hooks/firestore";
 
@@ -10,9 +10,9 @@ const AddToCartButton = ({ productData }: any) => {
 
     const addToCart = async () => {
         try {
-            // if (!user) {
-            //     throw new Error('Login first');
-            // } else {
+            if (!user) {
+                throw new Error('Login first');
+            } else {
             await setDoc(doc(db, 'cart', `${productData.productId}`), {
                 productId: productData.productId,
                 productName: productData.productName,
@@ -24,11 +24,24 @@ const AddToCartButton = ({ productData }: any) => {
                 createdAt: serverTimestamp(),
                 // uid: user.uid
             });
-            // }
+            }
         } catch (error) {
             alert(error);
         }
     }
+
+    const removeFromCart = async () => {
+        try {
+            if (!user) {
+                throw new Error('Login first');
+            } else {
+            await deleteDoc(doc(db, `cart/${productData.productId}`));
+            }
+        } catch (error) {
+            alert(error);
+        }
+    }
+
 
     return (
         <div>
@@ -42,6 +55,7 @@ const AddToCartButton = ({ productData }: any) => {
                 </button>
             ) : (
                 <button
+                    onClick={removeFromCart}
                     type="button"
                     className="max-w-xs flex-1 bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500 sm:w-full"
                 >
