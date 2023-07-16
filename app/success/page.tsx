0f3d27ec22668bc984ac6c749c0727261e4e09e6
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-17 00:26:43 +0530

    Whastapp Button functionality for any query.

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { CheckCircleIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
-const page = () => {
-  return (
-    <div>Success Page</div>
-  )
-}
+export default function PaymentSuccessCard() {
+    const [isAnimating, setIsAnimating] = useState(true);
 
-export default page
+    const animationVariants = {
+        hidden: {
+            opacity: 0,
+            y: '100%',
+        },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+                ease: 'easeInOut',
+            },
+        },
+    };
+
+    return (
+        <div className='w-full p-2'>
+            <motion.div
+                initial="hidden"
+                animate={isAnimating ? 'visible' : 'hidden'}
+                variants={animationVariants}
+                className="bg-white rounded-lg shadow-md overflow-hidden w-full mx-auto"
+            >
+                <div className="flex justify-center items-center bg-green-100 py-8">
+                    <CheckCircleIcon className="h-12 w-12 text-green-500" />
+                </div>
+                <div className="p-6">
+                    <h2 className="text-lg font-bold mb-2">Order Confirmation</h2>
+                    <p className="text-gray-500 text-sm mb-4">
+                        Thank you for your order! Your order details are below.
+                    </p>
+             
+                    <Link href={'/'}>
+                        <p
+                            className="bg-green-500 text-white font-semibold py-2 px-4 rounded hover:bg-green-600 transition-colors duration-300"
+
+                        >
+                            Go To Order Page
+                        </p>
+                    </Link>
+                </div>
+            </motion.div>
+        </div>
+    )
+};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,65 +10,75 @@ import { auth, db } from "@/firebase/firebaseConfig";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { collection } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import WhatsappButton from "./WhatsappButton";
 
 const Navbar = () => {
-    const [open, setOpen] = useState<boolean>(false)  //Handles the cart sidebar
-    const [user] = useAuthState(auth);
-    const [cartData,loading] = useCollectionData(collection(db,'cart'))
-    let cartLength = cartData?.filter((data)=>data?.uid === user?.uid)?.length || 0
+  const [open, setOpen] = useState<boolean>(false); //Handles the cart sidebar
+  const [user] = useAuthState(auth);
+  const [cartData, loading] = useCollectionData(collection(db, "cart"));
+  let cartLength =
+    cartData?.filter((data) => data?.uid === user?.uid)?.length || 0;
 
-    return (
-        <Disclosure as="nav" className="bg-white shadow sticky top-0 left-0 right-0 z-20">
-            <div className="max-w-5xl mx-auto xl:px-0 px-2">
-                <div className="flex justify-between h-16">
-                    {/* NavLinks */}
-                    <div className="flex">
-                        <Link href="/" className="flex-shrink-0 flex items-center ">
-                            <img
-                                className="h-10 w-10 rounded-full"
-                                src="https://chat.openai.com/_next/image?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa%2FAAcHTtd9M6KBOGZjnk94PFAEIGX-t7nDfWROVK17Mm8-ZMI%3Ds96-c&w=48&q=75"
-                            />
-                        </Link>
-                        <div className="hidden lg:ml-5 lg:flex lg:space-x-8 pt-1">
-                            <Link
-                                href="/iwatchUltra"
-                                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
-                            >
-                                IWATCH ULTRA
-                            </Link>
-                            <Link
-                                href="/airpod"
-                                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
-                            >
-                                AIRPOD
-                            </Link>
-                            <Link
-                                href="/tshirts"
-                                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
-                            >
-                                T-SHIRTS
-                            </Link>
-                        </div>
-                    </div>
-                    <Searchbar />
-                    <ProfileDropdown />
-                    <div className="group outline-none flex items-center pl-2 cursor-pointer">
-                        <div className="group relative">
-                            <ShoppingBagIcon
-                                onClick={() => { setOpen(true) }}
-                                className="flex-shrink-0 outline-none text-gray-400 group-hover:text-gray-500 h-7 w-7 cursor-pointer"
-                                aria-hidden="true"
-                            />
-                            <span className="absolute animate-pulse top-0 right-1 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs font-semibold">
-                              {cartLength}
-                            </span>
-                        </div>
-                    </div>
-                    <Cart open={open} setOpen={setOpen} />
-                </div>
+  return (
+    <Disclosure
+      as="nav"
+      className="bg-white shadow sticky top-0 left-0 right-0 z-20"
+    >
+      <div className="max-w-5xl mx-auto xl:px-0 px-2">
+        <div className="flex justify-between h-16">
+          {/* NavLinks */}
+          <div className="flex">
+            <Link href="/" className="flex-shrink-0 flex items-center ">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://chat.openai.com/_next/image?url=https%3A%2F%2Flh3.googleusercontent.com%2Fa%2FAAcHTtd9M6KBOGZjnk94PFAEIGX-t7nDfWROVK17Mm8-ZMI%3Ds96-c&w=48&q=75"
+              />
+            </Link>
+            <div className="hidden lg:ml-5 lg:flex lg:space-x-8 pt-1">
+              <Link
+                href="/iwatchUltra"
+                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
+              >
+                IWATCH ULTRA
+              </Link>
+              <Link
+                href="/airpod"
+                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
+              >
+                AIRPOD
+              </Link>
+              <Link
+                href="/tshirts"
+                className="border-transparent text-gray-500  hover:text-gray-700 inline-flex items-center px-1 pt-1 text-md font-medium"
+              >
+                T-SHIRTS
+              </Link>
             </div>
-        </Disclosure>
-    );
+          </div>
+          <Searchbar />
+          <ProfileDropdown />
+          <div className="group outline-none flex items-center pl-2 cursor-pointer">
+            <div className="group relative">
+              <ShoppingBagIcon
+                onClick={() => {
+                  setOpen(true);
+                }}
+                className="flex-shrink-0 outline-none text-gray-400 group-hover:text-gray-500 h-7 w-7 cursor-pointer"
+                aria-hidden="true"
+              />
+              <span className="absolute animate-pulse top-0 right-1 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs font-semibold">
+                {cartLength}
+              </span>
+            </div>
+          </div>
+
+          <WhatsappButton />
+
+          <Cart open={open} setOpen={setOpen} />
+        </div>
+      </div>
+    </Disclosure>
+  );
 };
 
 export default Navbar;
