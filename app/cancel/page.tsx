8008db80c61eb62e1cef66a5fb68117cfa25366a
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-18 05:05:19 +0530

    Protect success and cancel order route.

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,10 +1,27 @@
 'use client'
 import Lottie from "lottie-react";
 import loadingAnimation from "../../public/serverError.json";
+import { auth } from "@/firebase/firebaseConfig";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 export default function page() {
+    const [user] = useAuthState(auth);
+    const router = useRouter()
+    useEffect(() => {
+        if (user == null) {
+            router.push("/");
+            return;
+        }
+    }, [user]);
+
+    if (!user) {
+        return null;
+    }
+
     return (
         <div className="w-full flex items-center justify-center h-[70vh] overflow-hidden flex-col">
-                <h1 className="text-3xl font-bold px-4 text-center">❌ Oops! Your Order Was cancel.</h1>
+            <h1 className="text-3xl font-bold px-4 text-center">❌ Oops! Your Order Was cancel.</h1>
             <Lottie style={{ height: 300 }} animationData={loadingAnimation} loop={true} />
         </div>
     )
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -6,25 +6,39 @@ import { collection, deleteDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function page() {
-    const [user] = useAuthState(auth);
-    const cartsRef = collection(db, `users/${user?.uid}/cart`);
-    const [cartSnapshots, loading] = useCollection(cartsRef);
-    useEffect(() => {
-        const deleteCartItems = async () => {
-          if (!loading && cartSnapshots && user) {
-            const deletionPromises = cartSnapshots.docs.map((doc) => deleteDoc(doc?.ref));
-            await Promise.all(deletionPromises);
-          }
-        };
-      
-        deleteCartItems();
-      }, [cartSnapshots, user]);
-    
-    return (
-        <div className="w-full flex items-center justify-center h-[70vh] overflow-hidden flex-col">
-                <h1 className="text-3xl font-bold px-4 text-center">✅ Congratulations! Your Order Was Successful.</h1>
-            <Lottie style={{ height: 300 }} animationData={loadingAnimation} loop={true} />
-        </div>
-    )
+  const [user] = useAuthState(auth);
+  const router = useRouter()
+  const cartsRef = collection(db, `users/${user?.uid}/cart`);
+  const [cartSnapshots, loading] = useCollection(cartsRef);
+
+  useEffect(() => {
+    if (user == null) {
+      router.push("/");
+      return;
+    }
+
+    const deleteCartItems = async () => {
+      if (!loading && cartSnapshots && user) {
+        const deletionPromises = cartSnapshots.docs.map((doc) => deleteDoc(doc?.ref));
+        await Promise.all(deletionPromises);
+      }
+    };
+
+    deleteCartItems();
+  }, [user, cartSnapshots]);
+
+  if (!user) {
+    // user is signed out or still being checked.
+    // don't render anything
+    return null;
+  }
+
+  return (
+    <div className="w-full flex items-center justify-center h-[70vh] overflow-hidden flex-col">
+      <h1 className="text-3xl font-bold px-4 text-center">✅ Congratulations! Your Order Was Successful.</h1>
+      <Lottie style={{ height: 300 }} animationData={loadingAnimation} loop={true} />
+    </div>
+  )
 }
